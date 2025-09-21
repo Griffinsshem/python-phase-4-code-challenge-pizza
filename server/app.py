62d797a928be1335f0b4cfdce56fdd603e28f8b9
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-09-21 07:14:28 +0300

    feat: complete code challenge

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
 import os
 
@@ -14,7 +14,6 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
 api = Api(app)
@@ -23,6 +22,55 @@ api = Api(app)
 @app.route("/")
 def index():
     return "<h1>Code challenge</h1>"
+
+
+@app.route("/restaurants", methods=["GET"])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    return jsonify([restaurant.to_dict(only=("id", "name", "address")) for restaurant in restaurants])
+
+
+@app.route("/restaurants/<int:id>", methods=["GET"])
+def get_restaurant_by_id(id):
+    restaurant = Restaurant.query.get(id)
+    if not restaurant:
+        return jsonify({"error": "Restaurant not found"}), 404
+    return jsonify(restaurant.to_dict(rules=["restaurant_pizzas", "restaurant_pizzas.pizza"]))
+
+
+@app.route("/restaurants/<int:id>", methods=["DELETE"])
+def delete_restaurant_by_id(id):
+    restaurant = Restaurant.query.get(id)
+    if not restaurant:
+        return jsonify({"error": "Restaurant not found"}), 404
+    db.session.delete(restaurant)
+    db.session.commit()
+    return "", 204
+
+
+@app.route("/pizzas", methods=["GET"])
+def get_pizzas():
+    pizzas = Pizza.query.all()
+    return jsonify([pizza.to_dict(only=("id", "name", "ingredients")) for pizza in pizzas])
+
+
+@app.route("/restaurant_pizzas", methods=["POST"])
+def create_restaurant_pizza():
+    data = request.get_json()
+
+    try:
+        new_rp = RestaurantPizza(
+            price=data["price"],
+            pizza_id=data["pizza_id"],
+            restaurant_id=data["restaurant_id"]
+        )
+        db.session.add(new_rp)
+        db.session.commit()
+
+        return jsonify(new_rp.to_dict(rules=["pizza", "restaurant"])), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"errors": ["validation errors"]}), 400
 
 
 if __name__ == "__main__":
